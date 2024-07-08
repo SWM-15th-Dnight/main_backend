50e1c6e626311af3e8debc3b1506c8a62e7cd855
inmonim <cat1083014@gmail.com>
2024-07-08 15:52:55 +0900

    chore : init spring boot project

---
+++ src/main/kotlin/com/calinify/calinify/CalinifyApplication.kt
@@ -0,0 +1,11 @@
+package com.calinify.calinify
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CalinifyApplication
+
+fun main(args: Array<String>) {
+	runApplication<CalinifyApplication>(*args)
+}
---
+++ src/test/kotlin/com/calinify/calinify/CalinifyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.calinify.calinify
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CalinifyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
