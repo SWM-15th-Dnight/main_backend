25a3d2f0137d06153a6de731843b7dd60be0d4bf
inmonim <cat1083014@gmail.com>
2024-08-05 23:00:51 +0900

    feat : event update 구현

--- src/main/kotlin/com/Dnight/calinify/event/controller/EventController.kt
+++ src/main/kotlin/com/Dnight/calinify/event/controller/EventController.kt
@@ -3,26 +3,37 @@ package com.dnight.calinify.event.controller
 import com.dnight.calinify.config.basicResponse.BasicResponse
 import com.dnight.calinify.config.basicResponse.ResponseCode
 import com.dnight.calinify.event.dto.request.EventCreateRequestDTO
+import com.dnight.calinify.event.dto.request.EventUpdateRequestDTO
 import com.dnight.calinify.event.dto.response.EventResponseDTO
 import com.dnight.calinify.event.service.EventService
+import jakarta.validation.Valid
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("api/v1/event")
+@Validated
 class EventController(
     val eventService: EventService
 ) {
     @GetMapping("/{eventId}")
-    fun getEvent(@PathVariable eventId: Long) : BasicResponse<EventResponseDTO> {
-        val eventResponse : EventResponseDTO = eventService.getEventById(eventId)
+    fun getEvent(@PathVariable eventId: Long): BasicResponse<EventResponseDTO> {
+        val eventResponse: EventResponseDTO = eventService.getEventById(eventId)
 
         return BasicResponse.ok(eventResponse, ResponseCode.ResponseSuccess)
     }
 
     @PostMapping("/form")
-    fun createFormEvent(@RequestBody eventCreateDTO : EventCreateRequestDTO) : BasicResponse<EventResponseDTO> {
-        val eventCreateResponse : EventResponseDTO = eventService.createEvent(eventCreateDTO)
+    fun createFormEvent(@Valid @RequestBody eventCreateDTO: EventCreateRequestDTO): BasicResponse<EventResponseDTO> {
+        val eventCreateResponse: EventResponseDTO = eventService.createEvent(eventCreateDTO)
 
         return BasicResponse.ok(eventCreateResponse, ResponseCode.CreateSuccess)
+    }
+
+    @PutMapping("/")
+    fun updateEvent(@Valid @RequestBody eventUpdateRequestDTO: EventUpdateRequestDTO): BasicResponse<Long> {
+        val eventId = eventService.updateEvent(eventUpdateRequestDTO)
+
+        return BasicResponse.ok(eventId, ResponseCode.UpdateSuccess)
     }
 }
