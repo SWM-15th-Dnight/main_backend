68ab269e4c97a90bfaebcd002ccf5d9f06e3aedf
inmonim <cat1083014@gmail.com>
2024-08-13 21:45:55 +0900

    feat : inputType crud 생성

--- src/main/kotlin/com/Dnight/calinify/common/inputType/InputTypeRepository.kt
+++ src/main/kotlin/com/Dnight/calinify/common/inputType/InputTypeRepository.kt
@@ -2,4 +2,4 @@ package com.dnight.calinify.common.inputType
 
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface InputTypeRepository : JpaRepository<InputTypeEntity, Long>
+interface InputTypeRepository : JpaRepository<InputTypeEntity, Int>
