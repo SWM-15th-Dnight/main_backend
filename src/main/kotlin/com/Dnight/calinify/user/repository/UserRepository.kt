14061d82047bc94cb967261ab9ed6098161f771b
inmonim <cat1083014@gmail.com>
2024-08-14 20:07:37 +0900

    feat : 회원가입 및 로그인 단계 확인 로직 추가

--- src/main/kotlin/com/Dnight/calinify/user/repository/UserRepository.kt
+++ src/main/kotlin/com/Dnight/calinify/user/repository/UserRepository.kt
@@ -4,5 +4,8 @@ import com.dnight.calinify.user.entity.UserEntity
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface UserRepository : JpaRepository<UserEntity, Long> {
+
+    fun existsByEmail(userEmail: String): Boolean
+
     fun findByEmail(userEmail: String): UserEntity?
 }
